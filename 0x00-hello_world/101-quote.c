de9eeab10d02b89e82cab4cacadf6a2e165c7fc2
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-06 00:31:39 +0000

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,9 @@
  */
 int main(void)
 {
-	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(
+		stdout,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	);
 	return (1);
 }
