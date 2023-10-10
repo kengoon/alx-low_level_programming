9112f833408ad45ed504da94945e7ed76c984a2e
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-10 02:07:17 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main: Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	int n;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			if (n > ch)
+			{
+				putchar(ch);
+				putchar(n);
+				if (ch != 56 || n != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+putchar(10); /* this is an ascii code for new line*/
+
+return (0);
+}
