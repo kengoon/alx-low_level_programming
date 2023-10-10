aec9a0fca42142cf5d2c1a857c7e6fb2557db94a
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-10 02:26:41 +0000

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 47; i <= 57; ++i)
+	{
+		for (j = i + 1; j <= 57; ++j)
+		{
+			for (k = j + 1; k < 57; ++k)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i != 55 || j != 56 || k != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
