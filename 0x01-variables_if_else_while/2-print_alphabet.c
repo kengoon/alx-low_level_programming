73dba2fc0f2a9a2704f2919a3e9068b4bb5aff21
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-10 01:01:44 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Return
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
