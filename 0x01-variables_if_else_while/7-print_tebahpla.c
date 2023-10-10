714d421fa259da6e496f07b67e96891c2c7597c7
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-10 01:37:23 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Return
+ */
+int main(void)
+{
+	int acii;
+
+	for (ascii = 122; ch >= 97; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
