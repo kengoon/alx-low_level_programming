6078be8971b26fe18a0a988ef789f01b0d0c28b1
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-10 01:33:03 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
