4cf56583b2754099321c6056ba239b6b243591c5
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-10 01:22:26 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
