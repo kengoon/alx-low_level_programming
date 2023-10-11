5d31a40e8c5ca04090b3949fb1d195c81ec27dc2
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-11 00:38:26 +0000

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Adds two values
- * @n1: The first number to be computed
- * @n2: The second number to be computed
+ * @num1: The first number to be computed
+ * @num2: The second number to be computed
  *
  * Return: Integer value
  */
