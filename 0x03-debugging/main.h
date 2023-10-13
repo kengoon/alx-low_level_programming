d7f880cf0c581335f141113ed37abfc3e5c4c03a
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-13 22:43:15 +0000

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
