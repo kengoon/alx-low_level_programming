e9049dd355300efc58764fb9bbd6d75725497ebc
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-13 17:19:42 +0000

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints hashes squares, followed by a new line.
+ * @size: size of the square.
+ * Return: no return.
+ */
+void print_square(int size)
+{
+	int dx, dy;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (dy = 0; dy < size; dy++)
+	{
+		for (dx = 0; dx < size; dx++)
+			_putchar('#');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
