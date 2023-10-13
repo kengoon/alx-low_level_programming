720e6e553de27283c763374ea23d5dc50d14713f
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-13 16:34:20 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
