62cf95992404752c5012d645e52315ee4c939e8a
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-19 23:34:04 +0000

    1-stncat.c

---
+++ 0x06-pointers_arrays_strings/1-stncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates n bytes from a string to another
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes of str to concatenate
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+	return dest;
+}
