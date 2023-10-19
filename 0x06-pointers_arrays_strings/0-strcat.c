722afe9d65b736d4d786c203b14eb9f008e1b130
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-19 23:11:46 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int m, n;
+	int i, j;
 
-	m = 0;
-	n = 0;
-
-	while (dest[m] != '\0')
-		m++;
-
-	while (src[n] != '\0')
+	i = 0;
+	for (j = 0; dest[j] != '\0'; j++)
+		;
+	while (src[i] != '\0')
 	{
-		dest[m] = src[n];
-		m++;
-		n++;
+		dest[j + i] = src[i];
+		i++;
 	}
-
-	dest[m] = '\0';
-
 	return (dest);
 }
+
