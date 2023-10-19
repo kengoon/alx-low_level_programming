f419da29ecf270bc176beb065accdfa9f4bfa514
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-19 23:39:52 +0000

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,6 +17,6 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
-	return dest;
+	return (dest);
 }
 
