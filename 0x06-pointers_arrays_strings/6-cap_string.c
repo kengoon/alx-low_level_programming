7892c946bc9834ce1afb6b8407408130339816b8
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-20 00:35:14 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,15 +5,18 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0, j;
+	int i, j;
 	char sep[] = " \t\n,;.!?\"(){}";
 
-	while (s[i])
+	i = 1;
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= ('a' - 'A');
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z' && (i == 0 || strchr(sep, s[i - 1])))
-			s[i] -= 32;
+		for (j = 0; sep[j] != '\0'; j++)
+			if (s[i - 1] == sep[j] && (s[i] >= 'a' && s[i] <= 'z'))
+				s[i] -= ('a' - 'A');
 		i++;
 	}
 	return (s);
 }
-
