d1de15a6f91580f1ecfdc05084b93832f1a6941d
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-26 03:42:35 +0000

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
+}
+
