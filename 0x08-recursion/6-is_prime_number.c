24ad61021c3846b63f1837084fe72e54ccbcff19
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-26 04:49:48 +0000

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: divisor.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime(unsigned int n, unsigned int d)i
+{
+	if (n % d == 0)
+		return (0);
+
+	if (d == n / 2)
+		return (1);
+
+	return (is_prime(n, d + 1));
+}
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, 2));
+}
+
