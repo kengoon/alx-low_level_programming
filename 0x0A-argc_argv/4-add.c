2986c44ce5e06bb604e7429dad055ea7bd191399
Kenechukwu Akubue <kengoon19@gmail.com>
2023-10-30 14:03:22 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	for (int i = 1; i < argc; i++)
+	{
+		char *ptr;
+		int num = strtol(argv[i], &ptr, 10);
+
+		if (*ptr != '\0' || num <= 0 || num > INT_MAX - sum)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", argc > 1 ? sum : 0);
+	return (0);
+}
+
