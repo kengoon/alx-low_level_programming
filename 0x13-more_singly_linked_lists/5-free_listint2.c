db355aa1e53998d448bc5b6f97d9c7f0d9e301e6
Kenechukwu Akubue <kengoon19@gmail.com>
2023-11-21 23:45:19 +0000

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function with one argument
+ * @head: pointer to head pointer of list
+ *
+ * Description: frees a list
+ * Return: na
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *traverse;
+
+	if (!head)
+		return;
+
+	while (*head)
+	{
+		traverse = (*head)->next;
+		free(*head);
+		*head = traverse;
+	}
+	*head = NULL;
+}
