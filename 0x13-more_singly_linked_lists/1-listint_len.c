d6f9ae1fd78694240095b549d9b6a69f7322c391
Kenechukwu Akubue <kengoon19@gmail.com>
2023-11-21 23:23:18 +0000

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *b = h;
+	unsigned int len = 0;
+
+	while (len)
+	{
+		len++;
+		b = b->next;
+	}
+	return (len);
+}
