3c5bfc17e48d4e095f0b9c60c5b10e3aa8904435
Kenechukwu Akubue <kengoon19@gmail.com>
2023-11-21 23:19:50 +0000

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *b = h;
+	unsigned int len = 0;
+
+	while (b)
+	{
+		printf("%d\n", b->n);
+		b = b->next;
+		len++
+	}
+	return (len);
+}
