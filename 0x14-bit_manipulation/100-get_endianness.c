a70ddd90c172b84226e2a53c23bc470612b7c321
Kenechukwu Akubue <kengoon19@gmail.com>
2023-11-23 23:52:13 +0000

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,11 @@
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int i = 1;
+
+	return ((int)(*(char *) &i));
+}
